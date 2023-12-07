46443cccaf8e468edfdf5d86d8d255560a391613
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-12-07 20:50:32 +0530

    made the footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="flex text-[20px] text-[#8f9292] font-medium p-5 justify-between">
+		<footer className="flex text-[20px] text-[#8f9292] font-medium p-5 md:justify-between md:flex-row flex-col justify-center items-center">
 			<p> © 2023 Abhinav Gupta. All rights reserved </p>
 			<p className="flex gap-2">
 				<a href="https://www.linkedin.com/in/abhinavgpt/">LinkedIn</a>
